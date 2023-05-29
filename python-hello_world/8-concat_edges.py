1c65b41284a7be02e2f47ffabc8b02c09c338776
Super User <ismaelmolina544@gmail.com>
2023-05-28 22:16:42 -0700

    Task 6 of the beginning of the python journey

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = "object-oriented programming with Python"
+str = str[str.index("object"):str.index("object") + 28] + str[str.index("with"):str.index("with") + 5] + str[tr.index("Python"):str.index("Python") + 6]
 print(str)
