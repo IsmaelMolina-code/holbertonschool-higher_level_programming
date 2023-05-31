ada5a3b5a324ce65f6c4b3f762c5413f612d2f46
root <ismaelmolina544@gmail.com>
2023-05-31 09:51:38 -0700

    Task 2 and 3 of loops & functions

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-import string
 
-alphabet = string.ascii_lowercase
-
-for letter in alphabet:
-    if letter != 'q' and letter != 'e':
-        print(letter, end='')
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end='')
