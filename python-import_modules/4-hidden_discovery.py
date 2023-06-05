7c2d11752116ce5e9932ec0f2f26ea03abbbd772
Super User <ismaelmolina544@gmail.com>
2023-06-04 20:01:22 -0700

    Task 4 of import and modules

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+
+    names = dir(hidden_4)  # saving all names in variable names
+    sorted_names = sorted(names)
+    for name in sorted_names:
+        if name[:2] != "__":
+            print("{}".format(name))
