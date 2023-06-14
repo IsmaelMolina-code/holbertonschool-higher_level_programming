33335753700c25fcdf18be752aa25cebf88264f4
root <ismaelmolina544@gmail.com>
2023-06-14 10:37:04 -0700

    Task 4 of OOP

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" How to create a class that finds the area of a Square"""
+
+
+class Square:
+    """ Creating the class Square with the private size attribute """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        area = self.__size * self.__size
+        return area
