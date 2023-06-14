3b598b705f2bb9798e2b839e36ed2181bee4a304
root <ismaelmolina544@gmail.com>
2023-06-14 07:23:32 -0700

    Task 2 of OOP

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" How to create a class with a private attribute"""
+
+
+class Square:
+    """ Creating the class Square with the private size attribute """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
