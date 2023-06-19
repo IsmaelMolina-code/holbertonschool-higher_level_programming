ac0db3a583668c774cec406806869f42b9d1d070
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-06-19 20:52:56 -0300

    Task 3 of import and modules

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    from sys import argv
-
-    num_args = len(argv) - 1
-    arg_list = argv[1:]
-
-    if num_args == 0:
-        print("0 arguments.")
-    else:
-        print("{} argument{}:".format(num_args, "s" if num_args > 1 else ""))
-        for i, arg in enumerate(arg_list):
-            print("{}: {}".format(i + 1, arg))
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
