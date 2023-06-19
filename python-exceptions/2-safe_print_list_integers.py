99c875f351c0ca45127d4f549b20506b8a0611c2
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-06-19 18:35:04 -0300

    Task 2 of exceptions control fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,13 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except IndexError:
-            pass
-        except TypeError:
-            pass
-        except ValueError:
-            pass
-        else:
             count += 1
+        except (ValueError, TypeError):
+            continue
     print()
     return count
