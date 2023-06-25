5007495df48829f26a36dfaa9fa08a327cc1a4f5
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-06-25 18:51:07 -0300

    Task 7 of I/O

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,11 +5,11 @@
 import sys
 
 if __name__ == '__main__':
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_f = __import__('5-save_to_json_file').save_to_json_f
+    load_from_json_f = __import__('6-load_from_json_file').load_from_json_f
 
     try:
-        my_list = load_from_json_file("add_item.json")
+        my_list = load_from_json_f("add_item.json")
     except FileNotFoundError:
         my_list = []
-    save_to_json_file(my_list + sys.argv[1:], "add_item.json")
+    save_to_json_f(my_list + sys.argv[1:], "add_item.json")
