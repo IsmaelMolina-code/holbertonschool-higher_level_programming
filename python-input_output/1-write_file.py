6dc7bcb4ad5b5aed0b171108849d42cfc1569337
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-06-25 18:20:50 -0300

    Task 1 of I/O

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,9 +3,10 @@
 
 
 def write_file(filename="", text=""):
+    """ Write a string to a text file (UTF8)  """
     chars = 0
     for chars in range(len(text)):
         chars += 1
-    with open(filename, "w") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
         return chars
