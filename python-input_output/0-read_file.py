be3c1f922b87b2a9a0f95ee8b15a3f00db543c14
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-06-25 18:10:41 -0300

    Task 0 of I/O

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Module to read a file """
+""" Prototype to read a file """
 
 
 def read_file(filename=""):
     """ Function to read a file """
     with open(filename, encoding="utf-8") as f:
-        return f.read()
+        print(f.read(), end="")
