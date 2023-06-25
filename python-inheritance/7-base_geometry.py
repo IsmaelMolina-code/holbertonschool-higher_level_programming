53dc36dd6bcc9ee4a4f394bde6753836b800f46e
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-06-25 03:18:45 -0300

    Task 7 of Inheritance

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" BaseGeometry class """
+
+
+class BaseGeometry:
+    """ Empty class """
+    def area(self):
+        """ Raises an exception """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validates value """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        return value
