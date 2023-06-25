a3b770107e03e4d88f3e726bbbf9ac32f7c88d2b
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-06-25 19:02:31 -0300

    Task 7 of I/O

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,4 +12,5 @@ if __name__ == '__main__':
         my_list = load_from_json_f("add_item.json")
     except FileNotFoundError:
         my_list = []
-    save_to_json_f(my_list + sys.argv[1:], "add_item.json")
+    my_list.extend(sys.argv[1:])
+    save_to_json_f(my_list, "add_item.json")
