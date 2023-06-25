b8eadb95ed659454ffa370f43104aabb54aeb870
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-06-25 02:59:33 -0300

    Task 6 of Inheritance

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" BaseGeometry class """
+
+
+class BaseGeometry:
+    """ Empty class """
+    def area(self):
+        """ Raises an exception """
+        raise Exception("area() is not implemented")
