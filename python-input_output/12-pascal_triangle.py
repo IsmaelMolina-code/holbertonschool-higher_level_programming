d0cc327e74b85e38af98b0dbca5ce4ac0a3adfe9
unknown <ismaelmolina544@gmail.com>
2023-06-25 23:58:19 -0300

    Task 12 of I/O

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Pascal's Triangle """
+
+
+def pascal_triangle(n):
+    """ Pascal's Triangle """
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    pascal = [[1], [1, 1]]
+    for i in range(2, n):
+        pascal.append([1])
+        for j in range(1, i):
+            pascal[i].append(pascal[i - 1][j - 1] + pascal[i - 1][j])
+        pascal[i].append(1)
+    return pascal
